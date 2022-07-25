17f6755fc90375370bfb79769b7fb28814bfd098
ngotiendat <datmau20@gmail.com>
2022-07-25 17:53:06 +0700

    base project

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+//
+const app = express();
+app.use(express.json());
+//
+
+//connect database
+mongoose.connect("mongodb://localhost:27017/AsmNodejs", () => {
+  console.log("successfully");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Kết nối thành công, cổng ${process.env.PORT}`);
+});
