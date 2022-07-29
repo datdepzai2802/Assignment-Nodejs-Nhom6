843aceaff16b97ca50e1a5f9663bf9448127edf6
tuananh-lc <anhdhtph16661@fpt.edu.vn>
2022-07-29 11:16:17 +0700

    #4 - Creat page HomePage

---
+++ src/views/site/components/FooterUser/user-footer.js
@@ -0,0 +1,107 @@
+const Footertd = {
+	render: () => {
+		return (
+            /*html*/`
+			<div class="bgfttd">
+             
+            <div class="md:grid md:grid-cols-5 md:gap-14 md:pl-[130px] md:pr-[200px] md:pt-12 md:pb-20">
+            <div class="pl30 pr30  pt30">
+        <img class="bannernhohp Reposehidden" src="https://cdn-dgcei.nitrocdn.com/QaFavQVnaqgHtiSsAelwGDKVguOuACXM/assets/static/optimized/rev-cf3fdbc/wp-content/uploads/2022/04/Property-1White_mobile.svg"  alt="">
+      
+      
+       
+        
+            <p class="fttex123tth  text-white md:w-[275px] md:pt-3">Năm 2020, ShopDunk trở thành đại lý ủy quyền của Apple. Chúng tôi phát triển chuỗi cửa hàng tiêu chuẩn và Apple Mono Store nhằm mang đến trải nghiệm tốt nhất về sản phẩm và dịch vụ của Apple cho người dùng Việt Nam.</p>
+
+            </div>
+          
+            <div class="md:pl-20 md:w-[300px] pl20 pr20">
+            <p class="pl10 pr10 text-white pb10 borderbt  cursor-pointer pt30">Sản phẩm</p>  <br>    
+            <p class="fttext Reposehidden ">iPhone</p> 
+            <p class="fttext Reposehidden ">iPad</p> 
+            <p class="fttext Reposehidden ">Mac</p> 
+            <p class="fttext Reposehidden ">Apple Watch</p> 
+            <p class="fttext Reposehidden ">Âm thanh</p> 
+            <p class="fttext Reposehidden ">Phụ kiện</p> 
+            <p class="fttext Reposehidden ">Dịch vụ</p> 
+            </div>
+
+                        <div class="md:pl-20 md:w-[300px] pl20 pr20">
+            <p class="pl10 pr10 text-white pb10 borderbt  cursor-pointer ">Thông tin</p> <br> 
+            <p class="fttext Reposehidden ">Giới thiệu</p> 
+            <p class="fttext Reposehidden ">Khuyến mại</p> 
+            <p class="fttext Reposehidden ">Bảo hành và sửa chữa</p> 
+            <p class="fttext Reposehidden ">Tuyển dụng</p> 
+            <p class="fttext Reposehidden ">Tin tức</p> 
+            <p class="fttext Reposehidden ">Check IMEI</p> 
+            <p class="fttext Reposehidden ">Phương thức thanh toán</p> 
+            <p class="fttext Reposehidden ">Gửi góp ý, khiếu nại</p> 
+            </div>
+
+
+            <div class="md:pl-20 md:w-[300px] pl20 pr20">
+            <p class="pl10 pr10 text-white pb10 borderbt  cursor-pointer ">Chính sách</p>    <br> 
+            <p class="fttext Reposehidden ">Trả góp</p> 
+            <p class="fttext Reposehidden ">Giao hàng</p> 
+            <p class="fttext Reposehidden ">Giao hàng (ZaloPay)</p> 
+            <p class="fttext Reposehidden ">Hủy giao dịch</p> 
+            <p class="fttext Reposehidden ">Đổi trả</p> 
+            <p class="fttext Reposehidden ">Bảo hành</p> 
+            <p class="fttext Reposehidden ">Giải quyết khiếu nại</p> 
+            <p class="fttext Reposehidden ">Bảo mật thông tin</p> 
+            </div>
+
+
+            <div class="md:pl-20 md:w-[300px] pl20 pr20">
+            <p class="pl10 pr10 text-white pb10 borderbt  cursor-pointer ">Địa chỉ</p>
+            <p class="fttext Reposehidden ">Tìm Store trên Google Map</p> 
+            <p class="fttext Reposehidden ">Hệ thống cửa hàng</p> <br> 
+            <p class="pl10 pr10 text-white pb10 borderbt  cursor-pointer ">Liên hệ</p>   <br> 
+            <p class="fttext1 Reposehidden ">Mua hàng: <a class="fttextnb" href="">1900.6626</a></p> 
+            <p class="fttext1 Reposehidden ">Khiếu nại: <a class="fttextnb" href="">0886.308.688</a></p> 
+            <p class="fttext1 Reposehidden ">Doanh nghiệp & Đối tác: <br>
+            <a class="fttextnb Reposehidden " href=""> 0822.688.668</a>
+            </p> 
+            
+            </div>
+
+
+                        </div>
+
+                        <div class="md:pl-[130px] md:pr-[130px] md:pb-8">
+                        <hr class="gachngnang md:pb-2 Reposehidden "> 
+                        
+
+                        <div class="pl20 pr20 pb30    md:grid md:grid-cols-2">
+                        <div class="">
+                        <p class="fttext">© 2016 Công ty Cổ Phần HESMAN Việt Nam GPDKKD: 0107465657 do Sở KH & ĐT TP. Hà Nội cấp ngày 08/06/2016. </p> 
+
+                        <p class="fttext">Địa chỉ: Số 76 Thái Hà, phường Trung Liệt, quận Đống Đa, thành phố Hà Nội, Việt Nam</p> 
+
+                        <p class="fttext">Đại diện pháp luật: PHẠM MẠNH HÒA | ĐT: 0247.305.9999 | Email: lienhe@shopdunk.com </p> 
+                        </div>
+
+                        <div class="">
+                        <img class="pt10  md:float-right md:pt-3" src="https://cdn-dgcei.nitrocdn.com/QaFavQVnaqgHtiSsAelwGDKVguOuACXM/assets/static/optimized/rev-cf3fdbc/wp-content/uploads/2022/01/bo-cong-thuong-1170x780-2.png" width="150px" alt="">
+
+                        </div>
+
+                        </div>
+
+                        
+
+
+                        </div>
+                        </div>
+                       
+                        
+                  
+
+                    
+				
+            `
+		)
+	}
+}
+
+export default Footertd
