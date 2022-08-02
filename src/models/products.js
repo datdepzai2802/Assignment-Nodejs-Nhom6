bcb16176a0fb9b12f01ed8f10c4bd893970ac76a
ngotiendat <datmau20@gmail.com>
2022-08-02 22:19:02 +0700

    #19 - fix address model product - users

--- src/models/products.js
+++ src/models/products.js
@@ -1,31 +1,31 @@
-import mongoose, {ObjectId} from "mongoose";
+import mongoose, { ObjectId } from "mongoose";
 
 const productSchema = mongoose.Schema({
-    name:{
-        type: String
-    },
-    price:{
-        type: Number
-    },
-    color:{
-        type: String
-    },
-    images:{
-        type: String
-    },
-    desc:{
-        type: String
-    },
-    sale:{
-        type: Number
-    },
-    feature:{
-        type: String
-    },
-    categoryId:{
-        type: ObjectId,
-        ref: 'category'
-    }
-})
+  name: {
+    type: String,
+  },
+  price: {
+    type: Number,
+  },
+  image: {
+    type: String,
+  },
+  descriptionShort: {
+    type: String,
+  },
+  descriptionLong: {
+    type: String,
+  },
+  sale: {
+    type: Number,
+  },
+  feature: {
+    type: String,
+  },
+  categoryId: {
+    type: ObjectId,
+    ref: "category",
+  },
+});
 
 export default mongoose.model("Products", productSchema);
--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { signin, signup } from "../controllers/auth";
+import { signin, signup, UsersGetAll } from "../controllers/auth";
 
 const router = express.Router();
 
-router.post('/signup', signup);
-router.post('/signin', signin);
+router.post("/signup", signup);
+router.post("/signin", signin);
+router.get("/users", UsersGetAll);
 
 export default router;
