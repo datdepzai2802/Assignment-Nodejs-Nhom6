17af648e3fe2bf884104dce03e0548c0b4313d16
tuananh-lc <anhdhtph16661@fpt.edu.vn>
2022-08-05 12:42:31 +0700

    #29 - update api controller +  router user

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -2,8 +2,8 @@ import Users from "../models/users";
 
 export const UsersGetAll = async (req, res) => {
   try {
-    const users = await Users.find();
-    res.status(200).json(users);
+    const user = await Users.find();
+    res.status(200).json(user);
   } catch (error) {
     return res.status(400).json({
       error: "Không có users",
@@ -11,24 +11,54 @@ export const UsersGetAll = async (req, res) => {
   }
 };
 
-export const UsersGetOne = async (req, res) => {
+export const UsersGetOne = async(req,res)=>{
   try {
-    // const user = await
+      const id = req.params.id;
+      const user = await Users.findOne({_id:id}).exec();
+     
+      res.json({
+        user
+      })
   } catch (error) {
-    return res.status(400).json({
-      error: "Không có users",
-    });
+      return res.status(400).json({
+          error: "Không tìm thấy user"
+      })
   }
-};
+}
 
 export const UsersRemove = async (req, res) => {
   try {
-    const users = await Users.findOneAndDelete(req.params.id).exec();
+    const user = await Users.findOneAndDelete(req.params.id).exec();
     // console.log(users);
-    res.status(200).json(users);
+    res.status(200).json(user);
   } catch (error) {
     return res.status(400).json({
       error: "Xóa users thất bại",
     });
   }
 };
+
+export const create = async(req,res)=>{
+  try {
+      const user = await new Users(req.body).save();
+      res.json(user);
+  } catch (error) {
+      return res.status(400).json({
+          error: "Không thêm được users"
+      })
+  }
+}
+
+export const update = async(req, res)=>{
+  try {
+      const id = req.params.id;
+      const user = await Users.findOneAndUpdate({_id:id}, req.body, {new:true}).exec();
+      res.json(user);
+  } catch (error) {
+      return res.status(400).json({
+          error: "Không sửa được user"
+      })
+  }
+}
+
+
